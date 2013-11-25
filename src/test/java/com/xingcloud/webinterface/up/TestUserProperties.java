23c6d808ce56b94dc15929cf7c4145c05d75846e
labelnolongerexists <labelnolongerexists@gmail.com>
2013-11-25 15:47:47 +0800

    Change mail info

--- src/test/java/com/xingcloud/webinterface/up/TestUserProperties.java
+++ src/test/java/com/xingcloud/webinterface/up/TestUserProperties.java
@@ -1,9 +1,12 @@
 package com.xingcloud.webinterface.up;
 
+import com.xingcloud.mysql.MySql_16seqid;
 import com.xingcloud.mysql.UserProp;
 import com.xingcloud.webinterface.exception.UserPropertyException;
-import com.xingcloud.webinterface.utils.UserPropertiesInfoManager;
 import org.junit.Test;
+
+import java.sql.SQLException;
+import java.util.List;
 
 /**
  * User: Z J Wu Date: 13-8-29 Time: 上午11:02 Package: com.xingcloud.webinterface.up
@@ -11,11 +14,12 @@ import org.junit.Test;
 public class TestUserProperties {
 
   @Test
-  public void testUP() throws UserPropertyException {
-    UserPropertiesInfoManager userPropertiesInfoManager = UserPropertiesInfoManager.getInstance();
-    UserProp up = userPropertiesInfoManager.getUserProp("age", "register_time");
-    System.out.println(up);
-    up = userPropertiesInfoManager.getUserProp("age", "register_time");
-    up = userPropertiesInfoManager.getUserProp("age", "first_pay_time");
+  public void testUP() throws UserPropertyException, SQLException {
+    MySql_16seqid manager = MySql_16seqid.getInstance();
+    List<UserProp> result = manager.getUserProps("thevideomate");
+    for (UserProp up : result) {
+      System.out.println(up.getPropName() + "|" + up.getPropAlias());
+    }
   }
+
 }
