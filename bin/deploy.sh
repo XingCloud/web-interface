b6d38bfd4703172f4be4bf1589f8eae69825376b
labelnolongerexists <labelnolongerexists@gmail.com>
2014-02-12 11:59:37 +0800

    Modified pom.xml dependency.

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -4,26 +4,31 @@ line="############################################"
 code_home=/home/hadoop/git_project_home/web-interface
 # deploy bin home
 scripts_home=${code_home}/bin/
-aid=dd
 # tomcat port
-tport=8081
+tport=$1
+# Artifact Id
+aid=dd
+# Web interface env
+xa_env="production"
+# Java binary
+java_bin=/usr/java/jdk1.7.0_25/
+
+if [ "" = "$1" ];then
+  tport=18080
+  # Tomcat home
+  tomcat_home=/home/hadoop/catalina/apache-tomcat-7.0.50.te
+else
+  echo "User defined tomcat port found($1)"
+  tport=$1
+  # Tomcat home
+  tomcat_home=/home/hadoop/catalina/apache-tomcat-7.0.50.wi
+fi
+
 if [ "" = "$2" ];then
   branch=master
 else
   echo "User defined branch found($2)"
   branch=$2
-fi
-
-if [ "8081" = ${1} ];then
-  xa_env="production"
-  # Tomcat home
-  tomcat_home=/home/hadoop/catalina/apache-tomcat-7.0.42.8081
-  tport=8081
-else
-  xa_env="pre_production"
-  # Tomcat home
-  tomcat_home=/home/hadoop/catalina/apache-tomcat-7.0.42.18080
-  tport=18080
 fi
 
 echo "[CHECK-POINT] - Begin deploying data driller web interface."
@@ -32,6 +37,7 @@ echo "[CODE-HOME] - "${code_home}
 echo "[CURRENT-BRANCH] - "${branch}
 echo "[XA_ENV] - "${xa_env}
 echo "[TOMCAT-PORT] - "${tport}
+echo "[JAVA-BIN] - "${java_bin}
 echo ${line}
 echo "[CHECK-POINT] - Update code from VCS"
 
@@ -47,20 +53,20 @@ else
 fi
 
 echo "[CHECK-POINT] - Packaging."
-mvn -f ${code_home}/pom.xml clean package -Dxa_env=${xa_env} -DskipTests=true
+mvn -f ${code_home}/pom.xml clean package -Dxa_env=${xa_env} -Dtport=${tport} -Daid=${aid} -DskipTests=true
 
 echo "[CHECK-POINT] - Shutdown tomcat."
 sh ${tomcat_home}/bin/shutdown.sh
-java_bin=/usr/java/jdk1.7.0_25/
+
 for((i=1;i<=10;i++));do
-    proc=`ps aux | grep ${java_bin} | grep tomcat | grep -v "grep" | grep ${tport} | awk '{print $2}'`
-    if [ "" = "$proc" ]
-    then
-        break
-    else
-        echo "Tomcat(${proc}) is not shutdown yet. Try again 3 second later($i)."
-        sleep 3
-    fi
+  proc=`ps aux | grep ${java_bin} | grep tomcat | grep -v "grep" | grep ${tport} | awk '{print $2}'`
+  if [ "" = "$proc" ]
+  then
+    break
+  else
+    echo "Tomcat(${proc}) is not shutdown yet. Try again 3 second later($i)."
+    sleep 3
+  fi
 done
 proc=`ps aux | grep ${java_bin} | grep tomcat | grep -v "grep" | grep ${tport} | awk '{print $2}'`
 if [ "" != "$proc" ];then
@@ -69,11 +75,11 @@ if [ "" != "$proc" ];then
 fi
 echo "Tomcat is shutdown."
 
-echo "[CHECK-POINT] - Clean application"
+echo "[CHECK-POINT] - Clean application - ${aid}"
 rm -rf ${tomcat_home}/webapps/${aid}
 rm -rf ${tomcat_home}/webapps/${aid}*.war
 
-echo "[CHECK-POINT] - Copy application"
+echo "[CHECK-POINT] - Copy application - ${aid}"
 cp ${code_home}/target/${aid}.war ${tomcat_home}/webapps
 echo "[CHECK-POINT] - Start web server."
 sh ${tomcat_home}/bin/startup.sh
