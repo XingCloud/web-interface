2d701d6d55cfc7bc24e3faa6710f1ce2b8ed57aa
labelnolongerexists <labelnolongerexists@gmail.com>
2014-03-13 11:36:15 +0800

    新功能有bug

--- src/test/java/com/xingcloud/webinterface/plan/common/TestCommonQueryLogicalPlanNoSeg.java
+++ src/test/java/com/xingcloud/webinterface/plan/common/TestCommonQueryLogicalPlanNoSeg.java
@@ -1,5 +1,6 @@
 package com.xingcloud.webinterface.plan.common;
 
+import com.xingcloud.qm.service.Submit;
 import com.xingcloud.webinterface.enums.CommonQueryType;
 import com.xingcloud.webinterface.enums.Interval;
 import com.xingcloud.webinterface.model.Filter;
@@ -19,17 +20,18 @@ public class TestCommonQueryLogicalPlanNoSeg extends TestLogicalPlanBase {
   @Test
   public void testBuildPlan() throws Exception {
     String name = "common.day.noseg.json";
-    FormulaQueryDescriptor fqd = new CommonFormulaQueryDescriptor("age", TEST_REAL_BEGIN_DATE, TEST_REAL_END_DATE,
-                                                                  "visit.*", null, Filter.ALL, "2013-03-10",
-                                                                  "2013-03-12", Interval.MIN5, CommonQueryType.NORMAL);
+    FormulaQueryDescriptor fqd = new CommonFormulaQueryDescriptor("sof-apptools", "2014-02-20", "2014-02-20",
+                                                                  "stat.connect.normal.finialsuccess.wifi.recordphone",
+                                                                  null, Filter.ALL, "2014-02-20", "2014-02-20",
+                                                                  Interval.DAY, CommonQueryType.NORMAL);
     SegmentEvaluator.evaluate(fqd);
     LogicalPlan logicalPlan = fqd.toLogicalPlain();
     String planString = Plans.DEFAULT_DRILL_CONFIG.getMapper().writeValueAsString(logicalPlan);
     System.out.println(planString);
     write2File(name, planString);
-//    Submit submit = (Submit) SERVICE;
-//    for (int i = 0; i < 1; i++) {
-//      submit.submit(fqd.getKey(), planString, Submit.SubmitQueryType.PLAN);
-//    }
+    Submit submit = (Submit) SERVICE;
+    for (int i = 0; i < 1; i++) {
+      submit.submit(fqd.getKey(), planString, Submit.SubmitQueryType.PLAN);
+    }
   }
 }
